e32dc76e804eebc3e4f3745c57d15e2073003424
FonCroy <FonCroy@rambler.ru>
2015-05-25 00:51:44 +0300

    SessionHelper

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,15 +12,15 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_new_contact(Contact(first_name="First", middle_name="Middle", last_name="Last", nickname="FML",
                                 company="Test Inc.", mobile_phone="+79205894179", email="test@gmail.com",
                                 year_of_birth="1978"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_new_contact(Contact(first_name="", middle_name="", last_name="", nickname="", company="", mobile_phone="",
                                 email="", year_of_birth=""))
-    app.logout()
+    app.session.logout()
