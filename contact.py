b3cb4d0103c544a473c4030260ed275f1fe7279b
FonCroy <FonCroy@rambler.ru>
2015-05-24 20:43:39 +0300

    Тест по созданию контакта

---
+++ contact.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, nickname, company, mobile_phone, email, day_of_birth,
+                 month_of_birth, year_of_birth):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.company = company
+        self.mobile_phone = mobile_phone
+        self.email = email
+        self.day_of_birth = day_of_birth
+        self.month_of_birth = month_of_birth
+        self.year_of_birth = year_of_birth
