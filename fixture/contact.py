c26e7c37842c006cdab79f0bf830fbcfd40293e8
FonCroy <FonCroy@rambler.ru>
2015-05-25 01:36:26 +0300

    GroupHelper and ContactHelper

---
+++ fixture/contact.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def create(self, contact):
+        wd = self.app.wd
+        # init new contact creation
+        wd.find_element_by_link_text("add new").click()
+        # fill new contact information
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.year_of_birth)
+        # submit new contact
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.return_to_home_page()
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,14 +13,14 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.add_new_contact(Contact(first_name="First", middle_name="Middle", last_name="Last", nickname="FML",
-                                company="Test Inc.", mobile_phone="+79205894179", email="test@gmail.com",
-                                year_of_birth="1978"))
+    app.contact.create(Contact(first_name="First", middle_name="Middle", last_name="Last", nickname="FML",
+                               company="Test Inc.", mobile_phone="+79205894179", email="test@gmail.com",
+                               year_of_birth="1978"))
     app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.add_new_contact(Contact(first_name="", middle_name="", last_name="", nickname="", company="", mobile_phone="",
-                                email="", year_of_birth=""))
+    app.contact.create(Contact(first_name="", middle_name="", last_name="", nickname="", company="", mobile_phone="",
+                               email="", year_of_birth=""))
     app.session.logout()
